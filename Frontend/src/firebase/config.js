0ad5c14cc901d97e378cbbe267aa9474dc39f5d9
Aditya2987 <aditya2987ahir@gmail.com>
2026-02-16 18:23:06 +0530

    Remove sensitive Firebase config values
    Cleared Firebase configuration values for security.

--- Frontend/src/firebase/config.js
+++ Frontend/src/firebase/config.js
@@ -3,13 +3,13 @@
 // Firebase is initialized in auth.js to prevent duplicate initialization
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7oECCTivTEv1SzxjQv7IV9UvqMKEcyAk",
-  authDomain: "storynexis.firebaseapp.com",
-  projectId: "storynexis",
-  storageBucket: "storynexis.firebasestorage.app",
-  messagingSenderId: "572375227554",
-  appId: "1:572375227554:web:3a67b10a682b60e7c84bfb",
-  measurementId: "G-S9B1FM8166"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 export default firebaseConfig;
